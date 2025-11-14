4f8f6fedc0f72805e011a6ad1466b35b15ae5139
Andre-Messias <andreluizmessias5@gmail.com>
2025-11-13 23:16:52 -0300

    Revert "Resolvendo problemas de serialização"
    This reverts commit 51d9fcfdb8407571a51580c129a95addbe11e079.

---
+++ src/Modelo/Fases/ScriptFase3.java
@@ -0,0 +1,30 @@
+package Modelo.Fases;
+
+/**
+ * @brief Script de eventos e spawns para a Fase 3 (placeholder).
+ */
+public class ScriptFase3  extends ScriptDeFase {
+
+    protected long proximoSpawnInimigo = 0;
+    protected long intervaloSpawnInimigo = 60;
+
+    @Override
+    public void atualizarInimigos(Fase fase) {
+    }
+
+    @Override
+    public void atualizarCenario(Fase fase, double velocidadeScroll) {
+    }
+
+    @Override
+    public void preencherCenarioInicial(Fase fase) {
+    }
+
+    @Override
+    public void carregarRecursos(Fase fase) {
+    }
+
+    @Override
+    public void relinkarRecursosDosElementos(Fase fase) {
+    }
+}

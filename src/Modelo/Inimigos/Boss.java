61d4c852cea2bf2cd776c530b8790e6ecfe97ac9
Alexandre Weber <84647460+LycalopX@users.noreply.github.com>
2025-11-14 01:08:31 -0300

    boss sobrevive a um ataque de bomba (uou)

--- src/Modelo/Inimigos/Boss.java
+++ src/Modelo/Inimigos/Boss.java
@@ -4,8 +4,18 @@ import Auxiliar.LootTable;
 
 public abstract class Boss extends Inimigo {
 
+    boolean isBombed = false;
+
     public Boss(String sNomeImagePNG, double x, double y, LootTable lootTable, double vida) {
         super(sNomeImagePNG, x, y, lootTable, vida);
+    }
+
+    public void setBombed(boolean bombed) {
+        isBombed = bombed;
+    }
+
+    public boolean isBombed() {
+        return this.isBombed;
     }
     
 }
